c93898e44357ec45fcf4a95af86605db93427b02
PJ Gray <pj@evertrue.com>
2013-12-30 17:28:54 -0500

    Correct aspect ratios!

--- SGSMultipeerVideo/SGSViewController.m
+++ SGSMultipeerVideo/SGSViewController.m
@@ -8,6 +8,7 @@
 
 #import "SGSViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <CoreImage/CoreImage.h>
 #import <malloc/malloc.h>
 #import <MultipeerConnectivity/MultipeerConnectivity.h>
 
@@ -20,14 +21,8 @@
 
 @property (weak, nonatomic) IBOutlet UIView *previewView;
 
-// Session management.
-@property (nonatomic) dispatch_queue_t sessionQueue; // Communicate with the session and other session objects on this queue.
+@property (nonatomic) dispatch_queue_t sampleQueue; // Communicate with the session and other session objects on this queue.
 @property (nonatomic) AVCaptureSession *captureSession;
-@property (nonatomic) AVCaptureDeviceInput *videoDeviceInput;
-@property (nonatomic) AVCaptureVideoDataOutput *videoDataOutput;
-
-@property BOOL deviceAuthorized;
-
 
 @end
 
@@ -48,216 +43,63 @@
     _advertiserAssistant = [[MCAdvertiserAssistant alloc] initWithServiceType:@"multipeer-video" discoveryInfo:nil session:_session];
     [_advertiserAssistant start];
 
+    
+    
 	// Create the AVCaptureSession
     self.captureSession = [[AVCaptureSession alloc] init];
 
 	// Setup the preview view
     AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
     captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    captureVideoPreviewLayer.frame = CGRectMake(0,0, 300, 300);
+    captureVideoPreviewLayer.frame = CGRectMake(0,0, 320, 320);
     [self.previewView.layer addSublayer:captureVideoPreviewLayer];
+
+    AVCaptureDevice *videoDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
-	// Check for device authorization
-	[self checkDeviceAuthorizationStatus];
-	
-	// Dispatch the rest of session setup to the sessionQueue so that the main queue isn't blocked.
-    self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL);
-	
-	dispatch_async(self.sessionQueue, ^{
-
-		NSError *error = nil;
-		
-		AVCaptureDevice *videoDevice = [SGSViewController deviceWithMediaType:AVMediaTypeVideo preferringPosition:AVCaptureDevicePositionBack];
-        
-		AVCaptureDeviceInput *videoDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:videoDevice error:&error];
-		
-		if (error)
-		{
-			NSLog(@"%@", error);
-		}
-		
-		if ([self.captureSession canAddInput:videoDeviceInput])
-		{
-			[self.captureSession addInput:videoDeviceInput];
-            self.videoDeviceInput = videoDeviceInput;
-		}
-				
-        AVCaptureVideoDataOutput *videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
-        if ([self.captureSession canAddOutput:videoDataOutput]) {
-            
-            if ([videoDevice lockForConfiguration:nil]) {
-                videoDevice.activeVideoMaxFrameDuration = CMTimeMake(1,10);
-                videoDevice.activeVideoMinFrameDuration = CMTimeMake(1,10);
-                [videoDevice unlockForConfiguration];
-            }
-            
-            videoDataOutput.alwaysDiscardsLateVideoFrames = YES;
-
-            [self.captureSession addOutput:videoDataOutput];
-            self.videoDataOutput = videoDataOutput;
-            [self.videoDataOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA] forKey:(NSString*)kCVPixelBufferPixelFormatTypeKey]];
-            [self.videoDataOutput setSampleBufferDelegate:self queue:self.sessionQueue];
-            
-        }
-	});
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-	dispatch_async([self sessionQueue], ^{
-		[self.captureSession startRunning];
-	});
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-//- (UIImage *)cropImage:(UIImage*) image withRect:(CGRect)rect {
-//    if (image.scale > 1.0f) {
-//        rect = CGRectMake(rect.origin.x * image.scale,
-//                          rect.origin.y * image.scale,
-//                          rect.size.width * image.scale,
-//                          rect.size.height * image.scale);
-//    }
-//    
-//    CGImageRef imageRef = CGImageCreateWithImageInRect(image.CGImage, rect);
-//    UIImage *result = [UIImage imageWithCGImage:imageRef scale:image.scale orientation:image.imageOrientation];
-//    CGImageRelease(imageRef);
-//    return result;
-//}
-
-- (UIImage *)imageByCropping:(UIImage *)image toSize:(CGSize)size
-{
-    double x = (image.size.width - size.width) / 2.0;
-    double y = (image.size.height - size.height) / 2.0;
+    AVCaptureDeviceInput *videoDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:videoDevice error:nil];
+    [self.captureSession addInput:videoDeviceInput];
     
-    CGRect cropRect = CGRectMake(x, y, size.height, size.width);
-    CGImageRef imageRef = CGImageCreateWithImageInRect([image CGImage], cropRect);
-    
-    UIImage *cropped =  [UIImage imageWithCGImage:imageRef scale:image.scale orientation:UIImageOrientationRight];
-
-    CGImageRelease(imageRef);
-    
-    return cropped;
-}
-
-+ (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
-        if ([[UIScreen mainScreen] scale] == 2.0) {
-            UIGraphicsBeginImageContextWithOptions(newSize, YES, 2.0);
-        } else {
-            UIGraphicsBeginImageContext(newSize);
-        }
-    } else {
-        UIGraphicsBeginImageContext(newSize);
+    AVCaptureVideoDataOutput *videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    if ([videoDevice lockForConfiguration:nil]) {
+        videoDevice.activeVideoMaxFrameDuration = CMTimeMake(1,10);
+        videoDevice.activeVideoMinFrameDuration = CMTimeMake(1,10);
+        [videoDevice unlockForConfiguration];
     }
-    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
+
+    self.sampleQueue = dispatch_queue_create("VideoSampleQueue", DISPATCH_QUEUE_SERIAL);
+    [videoDataOutput setSampleBufferDelegate:self queue:self.sampleQueue];
+    videoDataOutput.alwaysDiscardsLateVideoFrames = YES;
+    [self.captureSession addOutput:videoDataOutput];
+
+    [self.captureSession startRunning];
+
 }
 
-// Create a UIImage from sample buffer data
-- (UIImage *) imageFromSampleBuffer:(CMSampleBufferRef) sampleBuffer
-{
-    // Get a CMSampleBuffer's Core Video image buffer for the media data
-    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
-    // Lock the base address of the pixel buffer
-    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+- (UIImage*) cgImageBackedImageWithCIImage:(CIImage*) ciImage {
     
-    // Get the number of bytes per row for the pixel buffer
-    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef ref = [context createCGImage:ciImage fromRect:ciImage.extent];
+    UIImage* image = [UIImage imageWithCGImage:ref scale:[UIScreen mainScreen].scale orientation:UIImageOrientationRight];
+    CGImageRelease(ref);
     
-    // Get the number of bytes per row for the pixel buffer
-    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
-    // Get the pixel buffer width and height
-    size_t width = CVPixelBufferGetWidth(imageBuffer);
-    size_t height = CVPixelBufferGetHeight(imageBuffer);
-    
-    // Create a device-dependent RGB color space
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    
-    // Create a bitmap graphics context with the sample buffer data
-    CGContextRef context1 = CGBitmapContextCreate(baseAddress, width, height, 8,
-                                                  bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
-    
-    // Create a Quartz image from the pixel data in the bitmap graphics context
-    CGImageRef quartzImage = CGBitmapContextCreateImage(context1);
-    // Unlock the pixel buffer
-    CVPixelBufferUnlockBaseAddress(imageBuffer,0);
-    
-    // Free up the context and color space
-    CGContextRelease(context1);
-    CGColorSpaceRelease(colorSpace);
-    
-    // Create an image object from the Quartz image
-    //I modified this line: [UIImage imageWithCGImage:quartzImage]; to the following to correct the orientation:
-    UIImage *image =  [UIImage imageWithCGImage:quartzImage scale:[[UIScreen mainScreen] scale] orientation:UIImageOrientationRight];
-    
-    // Release the Quartz image
-    CGImageRelease(quartzImage);
-    
-    return [self imageByCropping:image toSize:CGSizeMake(300.0, 300.0)];
+    return image;
 }
-
 
 #pragma mark - AVCaptureVideoDataOutputSampleBufferDelegate
 
 - (void) captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
 
     if (_session.connectedPeers.count) {
-        UIImage* image = [self imageFromSampleBuffer:sampleBuffer];
-        NSData *data = UIImageJPEGRepresentation(image, 0.2);
+        CVImageBufferRef cvImage = CMSampleBufferGetImageBuffer(sampleBuffer);
+        CGRect cropRect = AVMakeRectWithAspectRatioInsideRect(CGSizeMake(320, 320), CGRectMake(0,0, CVPixelBufferGetWidth(cvImage),CVPixelBufferGetHeight(cvImage)) );
+        CIImage *ciImage = [[CIImage alloc] initWithCVPixelBuffer:cvImage];
+        CIImage* croppedImage = [ciImage imageByCroppingToRect:cropRect];
         
+        NSData *data = UIImageJPEGRepresentation([self cgImageBackedImageWithCIImage:croppedImage], 0.2);
         [_session sendData:data toPeers:_session.connectedPeers withMode:MCSessionSendDataReliable error:nil];
     }
 }
 
-
-#pragma mark - utilities
-
-- (void)checkDeviceAuthorizationStatus
-{
-	NSString *mediaType = AVMediaTypeVideo;
-	
-	[AVCaptureDevice requestAccessForMediaType:mediaType completionHandler:^(BOOL granted) {
-		if (granted) {
-			//Granted access to mediaType
-			self.deviceAuthorized = YES;
-		}
-		else {
-			//Not granted access to mediaType
-			dispatch_async(dispatch_get_main_queue(), ^{
-				[[[UIAlertView alloc] initWithTitle:@"AVCam!"
-											message:@"AVCam doesn't have permission to use Camera, please change privacy settings"
-										   delegate:self
-								  cancelButtonTitle:@"OK"
-								  otherButtonTitles:nil] show];
-                self.deviceAuthorized = NO;
-			});
-		}
-	}];
-}
-
-+ (AVCaptureDevice *)deviceWithMediaType:(NSString *)mediaType preferringPosition:(AVCaptureDevicePosition)position
-{
-	NSArray *devices = [AVCaptureDevice devicesWithMediaType:mediaType];
-	AVCaptureDevice *captureDevice = [devices firstObject];
-	
-	for (AVCaptureDevice *device in devices)
-	{
-		if ([device position] == position)
-		{
-			captureDevice = device;
-			break;
-		}
-	}
-	
-	return captureDevice;
-}
 
 #pragma mark - MCSessionDelegate Methods
 
